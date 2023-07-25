936b9c387758f67dc85a6d935b33d534ff5342d0
Tam Nguyen <tamnd@netacom.vn>
2023-07-25 21:41:30 +0700

    update latest version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.5"
+        kotlinCompilerExtensionVersion = "1.5.0"
     }
     packagingOptions {
         resources {

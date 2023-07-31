35b06053b670fa755eb45ca26af9d6edeac39fbe
Tam Nguyen <tamnd@netacom.vn>
2023-08-01 02:27:31 +0700

    Preview HomeScreen

--- feature/home/src/main/java/com/tmdb/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/tmdb/feature/home/HomeScreen.kt
@@ -1,8 +1,10 @@
 package com.tmdb.feature.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -17,11 +19,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tmdb.core.model.db.DbMovie
+import com.tmdb.core.model.fakeData.listDbMovieFakeData
+import com.tmdb.core.model.fakeData.listGenreFakeData
+import com.tmdb.core.model.fakeData.listMovieFakeData
+import com.tmdb.core.model.network.Genre
 import com.tmdb.core.model.network.Movie
 import com.tmdb.feature.home.view.CategoryView
 import com.tmdb.feature.home.view.PopularView
@@ -122,6 +130,69 @@ fun HomeScreen(
         }
     }
 
+    HomeScreenDetail(
+        totalMoviesRecommend = totalMoviesRecommend,
+        moviesRecommendation = moviesRecommendation,
+        genreList = genreList.value?.genres ?: emptyList(),
+        totalMoviesPopular = totalMoviesPopular,
+        moviesPopular = moviesPopular,
+        totalMoviesTopRated = totalMoviesTopRated,
+        moviesTopRated = moviesTopRated,
+        totalMoviesUpcoming = totalMoviesUpcoming,
+        moviesUpcoming = moviesUpcoming,
+        loadMoreRecommendation = {
+            pagingMoviesRecommend.value += 1
+            recommendViewModel.getTMDBRecommend(paging = pagingMoviesRecommend.value) { _movies ->
+                if (_movies?.listDbMovies?.isNotEmpty() == true) {
+                    moviesRecommendation.addAll(_movies.listDbMovies!!)
+                }
+            }
+        },
+        loadMorePopular = {
+            pagingMoviesPopular.value += 1
+            recommendViewModel.getTMDBPopular(paging = pagingMoviesPopular.value) { _movies ->
+                if (_movies?.results?.isNotEmpty() == true) {
+                    moviesPopular.addAll(_movies.results)
+                }
+            }
+        },
+        loadMoreTopRated = {
+            pagingMoviesTopRated.value += 1
+            recommendViewModel.getTMDBTopRated(paging = pagingMoviesTopRated.value) { _movies ->
+                if (_movies?.results?.isNotEmpty() == true) {
+                    moviesTopRated.addAll(_movies.results)
+                }
+            }
+        },
+        loadMoreUpcoming = {
+            pagingMoviesUpcoming.value += 1
+            recommendViewModel.getTMDBUpcoming(paging = pagingMoviesUpcoming.value) { _movies ->
+                if (_movies?.results?.isNotEmpty() == true) {
+                    moviesUpcoming.addAll(_movies.results)
+                }
+            }
+        },
+        navToMovieDetail = navToMovieDetail
+    )
+}
+
+@Composable
+fun HomeScreenDetail(
+    totalMoviesRecommend: MutableState<Int>,
+    moviesRecommendation: List<DbMovie>,
+    genreList: List<Genre>? = emptyList(),
+    totalMoviesPopular: MutableState<Int>,
+    moviesPopular: List<Movie>,
+    totalMoviesTopRated: MutableState<Int>,
+    moviesTopRated: List<Movie>,
+    totalMoviesUpcoming: MutableState<Int>,
+    moviesUpcoming: List<Movie>,
+    loadMoreRecommendation: () -> Unit,
+    loadMorePopular: () -> Unit,
+    loadMoreTopRated: () -> Unit,
+    loadMoreUpcoming: () -> Unit,
+    navToMovieDetail: (Int) -> Unit,
+) {
     Column(
         modifier = Modifier
             .padding(start = 10.dp, end = 10.dp)
@@ -150,57 +221,68 @@ fun HomeScreen(
                 navToMovieDetail.invoke(it)
             },
             loadMore = {
-                pagingMoviesRecommend.value += 1
-                recommendViewModel.getTMDBRecommend(paging = pagingMoviesRecommend.value) { _movies ->
-                    if (_movies?.listDbMovies?.isNotEmpty() == true) {
-                        moviesRecommendation.addAll(_movies.listDbMovies!!)
-                    }
-                }
+                loadMoreRecommendation.invoke()
             }
         )
 
-        CategoryView(listGenre = genreList.value?.genres ?: emptyList())
+        CategoryView(listGenre = genreList ?: emptyList())
 
         PopularView(
             totalMoviesPopular = totalMoviesPopular, movies = moviesPopular,
             navToMovieDetail = {
                 navToMovieDetail.invoke(it)
+            },
+            loadMore = {
+                loadMorePopular.invoke()
             }
-        ) {
-            pagingMoviesPopular.value += 1
-            recommendViewModel.getTMDBPopular(paging = pagingMoviesPopular.value) { _movies ->
-                if (_movies?.results?.isNotEmpty() == true) {
-                    moviesPopular.addAll(_movies.results)
-                }
-            }
-        }
+        )
 
         TopRatedView(
             totalMoviesTopRated = totalMoviesTopRated, movies = moviesTopRated,
             navToMovieDetail = {
                 navToMovieDetail.invoke(it)
+            },
+            loadMore = {
+                loadMoreTopRated.invoke()
             }
-        ) {
-            pagingMoviesTopRated.value += 1
-            recommendViewModel.getTMDBTopRated(paging = pagingMoviesTopRated.value) { _movies ->
-                if (_movies?.results?.isNotEmpty() == true) {
-                    moviesTopRated.addAll(_movies.results)
-                }
-            }
-        }
+        )
 
         UpcomingView(
             totalMoviesUpcoming = totalMoviesUpcoming, movies = moviesUpcoming,
             navToMovieDetail = {
                 navToMovieDetail.invoke(it)
+            },
+            loadMore = {
+                loadMoreUpcoming.invoke()
             }
-        ) {
-            pagingMoviesUpcoming.value += 1
-            recommendViewModel.getTMDBUpcoming(paging = pagingMoviesUpcoming.value) { _movies ->
-                if (_movies?.results?.isNotEmpty() == true) {
-                    moviesUpcoming.addAll(_movies.results)
-                }
-            }
-        }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewHomeDetailScreen() {
+    val totalMoviesRecommend: MutableState<Int> = remember { mutableStateOf(100) }
+    Box(
+        modifier = Modifier
+            .background(color = Color.White)
+            .fillMaxSize()
+    ) {
+        HomeScreenDetail(
+            totalMoviesRecommend = totalMoviesRecommend,
+            moviesRecommendation = listDbMovieFakeData,
+            genreList = listGenreFakeData,
+            totalMoviesPopular = totalMoviesRecommend,
+            moviesPopular = listMovieFakeData,
+            totalMoviesTopRated = totalMoviesRecommend,
+            moviesTopRated = listMovieFakeData,
+            totalMoviesUpcoming = totalMoviesRecommend,
+            moviesUpcoming = listMovieFakeData,
+            loadMoreRecommendation = { /*TODO*/ },
+            loadMorePopular = { /*TODO*/ },
+            loadMoreTopRated = { /*TODO*/ },
+            loadMoreUpcoming = { /*TODO*/ },
+            navToMovieDetail = {}
+        )
     }
 }
